5eede498e28af8074853a9ec27a59f3b7282c16d
nanbokku <29055086+nanbokku@users.noreply.github.com>
2020-10-05 23:59:08 +0900

    :sparkles: Add wipe effect

---
+++ Assets/Scripts/WipeEffect.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WipeEffect : MonoBehaviour
+{
+    [SerializeField]
+    private Material wipeEffect = null;
+    [SerializeField]
+    private Vector2 centerPosition = Vector2.zero;  // 円の中心位置
+    [SerializeField]
+    private float wipeSpeed = 1;                    // 円が小さくなる速度
+    [SerializeField]
+    private float minRadiusBeforeStopping = 1;      // 円が一旦停止する前の円の半径
+    [SerializeField]
+    private float stopTime = 0;                     // 円が一旦停止する時間
+    private Vector2 center = Vector2.zero;
+    private float radius = 1;
+    private int centerPropertyId = 0;
+    private int radiusPropertyId = 0;
+    private float startStoppingTime = -1;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        UpdateMaterial();
+
+        Graphics.Blit(src, dest, wipeEffect);
+    }
+
+    void Awake()
+    {
+        centerPropertyId = Shader.PropertyToID("_Center");
+        radiusPropertyId = Shader.PropertyToID("_Radius");
+
+        center = centerPosition;
+        radius = 600;
+        startStoppingTime = -1;
+    }
+
+    void UpdateMaterial()
+    {
+        var r = radius - wipeSpeed * Time.deltaTime;
+        if (r < minRadiusBeforeStopping && startStoppingTime < 0)
+        {
+            startStoppingTime = Time.time;
+            return;
+        }
+
+        if (startStoppingTime >= 0 && Time.time - startStoppingTime <= stopTime)
+        {
+            return;
+        }
+
+        radius = r;
+
+        wipeEffect.SetVector(centerPropertyId, center);
+        wipeEffect.SetFloat(radiusPropertyId, radius);
+    }
+}
---
+++ Assets/Shaders/WipeCircle.shader
@@ -0,0 +1,33 @@
+﻿Shader "Unlit/WipeCircle"
+{
+    Properties
+    {
+        _MainTex ("MainTexure", 2D) = "white" {}
+        _Center ("Center", Vector) = (300,300, 0, 0)
+        _Radius ("Radius", Range(0,600)) = 1
+    }
+    SubShader
+    {
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+            fixed2 _Center;
+            fixed _Radius;
+
+            fixed4 frag (v2f_img i) : SV_Target
+            {
+                fixed dist = distance(_Center, i.uv * _ScreenParams.xy);
+
+                return lerp(fixed4(0,0,0,1), tex2D(_MainTex, i.uv), step(dist, _Radius));
+            }
+            ENDCG
+        }
+    }
+}
